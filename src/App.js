4b661971fed5f1f4e9d7b606870b079c194654e1
angel <angelsol299@hotmail.com>
2019-07-16 11:18:01 +0200

    seccion presupuestos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from './components/layout/Navbar';
 import Mains from './components/layout/Mains';
+import Presupuesto from './components/layout/Presupuesto';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Mains />
+      <Presupuesto />
     </div>
   );
 }
