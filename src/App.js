f4db267b144f87405654f09bd032e3b6c03664f4
angel <angelsol299@hotmail.com>
2019-07-25 23:55:37 +0200

    main page finished

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navbar from './components/layout/Navbar';
-import Mainmas from './components/layout/Mainmas';
+import Mains from './components/layout/Mains';
 import FullPage from './components/layout/FullPage';
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <Navbar />
       <div style={{ display: 'flex', flexDirection: 'column' }}>
-        <FullPage style={{ paddingTop: '-180px' }} />
+        <Mains />
       </div>
     </div>
   );
--- src/components/layout/Mains.js
+++ src/components/layout/Mains.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react';
 import main1 from '../pics/electricity.jpeg';
-import Button from '@material-ui/core/Button';
+import { Button, Container } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 const myHeader = {
@@ -40,6 +40,19 @@ const useStyles = makeStyles(theme => ({
       width: '260px !important',
       marginTop: '20px'
     }
+  },
+  myHeaderImg: {
+    width: '100%',
+    height: '100%',
+    position: 'absolute',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    background: 'rgba(0,0,0, 0.7)',
+    marginLeft: '-32px',
+    [theme.breakpoints.down('sm')]: {
+      marginLeft: '-16px'
+    }
   }
 }));
 
@@ -50,54 +63,55 @@ const myHeaderImg = {
   backgroundPosition: 'center',
   backgroundRepeat: 'no-repeat',
   backgroundSize: 'cover',
-  background: 'rgba(0,0,0, 0.7)',
-  marginTop: '-83px'
+  background: 'rgba(0,0,0, 0.7)'
 };
 
 const Mains = () => {
   const classes = useStyles();
   return (
-    <div style={myHeader}>
-      <img style={myHeaderImg} src={main1} alt="main image" />
-      <div
-        className={classes.buttonResponsive}
-        style={{
-          position: 'absolute',
+    <div className={classes.myHeaderImg}>
+      <Container maxWidth="lg">
+        <img style={myHeaderImg} src={main1} alt="main image" />
+        <div
+          className={classes.buttonResponsive}
+          style={{
+            position: 'absolute',
 
-          top: '50%',
-          display: 'flex',
-          justifyContent: 'space-between',
-          marginLeft: '-194px'
-        }}
-      >
-        <Button
-          style={{
-            backgroundColor: 'white',
-            color: 'black',
-            borderRadius: '15px',
-            width: '200px'
+            top: '50%',
+            display: 'flex',
+            justifyContent: 'space-between',
+            marginLeft: '-194px'
           }}
-          variant="contained"
-          color="secondary"
-          className={classes.button}
         >
-          Pregunta
-        </Button>
-        <Button
-          style={{
-            color: 'white',
-            borderRadius: '15px',
-            border: ' 2px solid white',
-            background: 'rgba(0,0,0,0.5)',
-            width: '200px'
-          }}
-          variant="outlined"
-          color="secondary"
-          className={classes.button}
-        >
-          Conócenos
-        </Button>
-      </div>
+          <Button
+            style={{
+              backgroundColor: 'white',
+              color: 'black',
+              borderRadius: '15px',
+              width: '200px'
+            }}
+            variant="contained"
+            color="secondary"
+            className={classes.button}
+          >
+            Pregunta
+          </Button>
+          <Button
+            style={{
+              color: 'white',
+              borderRadius: '15px',
+              border: ' 2px solid white',
+              background: 'rgba(0,0,0,0.5)',
+              width: '200px'
+            }}
+            variant="outlined"
+            color="secondary"
+            className={classes.button}
+          >
+            Conócenos
+          </Button>
+        </div>
+      </Container>
     </div>
   );
 };
